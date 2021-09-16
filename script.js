452f0c0073f455787d39d748cc1d5ea2a147e519
JohnPaulGeorgeRingo <90808663+JohnPaulGeorgeRingo@users.noreply.github.com>
2021-09-15 20:56:47 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+var pTag1 = document.createElement("p");
+    pTag1.textContent = '121';
+    pTag1.setAttribute("class", "list1")
+    pTag1.setAttribute("id", "parTag1")
+    document.getElementById('question').appendChild(pTag1);
+    var input1 = document.createElement("input");
+    input1.setAttribute("class", "input")
+    document.getElementById
