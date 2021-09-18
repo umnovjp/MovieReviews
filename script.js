6fbb455db83261e0972a5133947d9083f194302f
JohnPaulGeorgeRingo <90808663+JohnPaulGeorgeRingo@users.noreply.github.com>
2021-09-18 11:38:22 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+var pTag1 = document.createElement("p");
+    pTag1.textContent = '121';
+    pTag1.setAttribute("id", "list1")
+    pTag1.setAttribute("id", "parTag1")
+    document.getElementById('question').appendChild(pTag1);
+    var input1 = document.createElement("input");
+    input1.setAttribute("class", "input")
+    document.getElementById('parTag1').appendChild(input1);
+
+ 
