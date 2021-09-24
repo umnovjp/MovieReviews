5eb179e7299ff4c0ba47c7f2c785f6660e517976
JohnPaulGeorgeRingo <90808663+JohnPaulGeorgeRingo@users.noreply.github.com>
2021-09-24 16:54:29 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -10,15 +10,19 @@ var listOfMoviesImdbComedy = document.getElementById('boxOfDVDsComedy');
 var fetchImdbComedy = document.getElementById('comedyImdbButton');
 var listOfMoviesImdbAction = document.getElementById('boxOfDVDsAction');
 var fetchImdbAction = document.getElementById('actionImdbButton');
-var enterYear0 = document.getElementById('enterYear');
+// var enterYear0 = document.getElementById('enterYear');
 var movieTitle;
 var object1;
+var inputVal = '2021'
 //these five variables are created to display movies 5-9, 10-14, etc. They do not work yet
 var dramaCount = 0;
 var familyCount = 0;
 var thrillerCount = 0;
 var comedyCount = 0;
 var actionCount = 0;
+
+function getInputValue() {var inputVal = document.getElementById('myInput').value;
+console.log(inputVal)}
 
 fetchImdbDrama.addEventListener('click', getApiDataImdb);
 fetchImdbFamily.addEventListener('click', getApiDataImdb);
@@ -27,14 +31,17 @@ fetchImdbComedy.addEventListener('click', getApiDataImdb);
 fetchImdbAction.addEventListener('click', getApiDataImdb);
 // enterYear0.addEventListener('click', updateYear);
 
+
 function getApiDataImdb(event) {
-  var buttonInput = console.log(event.currentTarget);
+ // var buttonInput = console.log(event.currentTarget);
   // document.writeln(String(buttonInput));
   var genre = event.currentTarget.value;
   console.log(genre);
-  var year = 2000;
+// if (inputVal = '') {inputVal = '2021'}
+// else { }
+  console.log(inputVal)
   
-  var requestURL = 'https://data-imdb1.p.rapidapi.com/movie/byYear/' + year + '/byGen/' + genre + '/';
+  var requestURL = 'https://data-imdb1.p.rapidapi.com/movie/byYear/' + inputVal + '/byGen/' + genre + '/';
   fetch(requestURL, {
     "method": "GET", "headers": {
       "x-rapidapi-host": "data-imdb1.p.rapidapi.com",
@@ -43,7 +50,6 @@ function getApiDataImdb(event) {
   })
     .then(function (response) {
       return response.json();
-      // console.log(response.status);
     })
     .then(function (data) {
       console.log(data);
@@ -58,7 +64,8 @@ function getApiDataImdb(event) {
         movieTitleFormatted2 = movieTitleFormatted1.replace(" ", "%20");
         movieTitleFormatted3 = movieTitleFormatted2.replace(" ", "%20");
         movieTitleFormatted4 = movieTitleFormatted3.replace(" ", "%20");
-        var requestURLOmdb = 'http://www.omdbapi.com/?i=tt3896198&apikey=bf124b81&t=' + movieTitleFormatted4 + '&plot=full' 
+        var requestURLOmdb = 'http://www.omdbapi.com/?i=tt3896198&apikey=bf124b81&t=' + movieTitleFormatted4 + '&plot=full'
+        // var requestPosterOmdb = 'http://img.omdbapi.com/?i=tt3896198&apikey=bf124b81&t=' + movieTitleFormatted4 + '&plot=full' 
         // end of ugly method
       //  console.log(requestURLOmdb + ' ' + genre);
         //start second fetch
@@ -74,7 +81,8 @@ function getApiDataImdb(event) {
             if (genre == 'Drama') {
               var movieName1 = document.createElement('p');
               movieName1.innerHTML = object1.Title;
-              document.getElementById('boxOfDVDsDrama').appendChild(movieName1)
+              document.getElementById('boxOfDVDsDrama').appendChild(movieName1);
+              
               var movieData = document.createElement('p');
               movieData.innerHTML = 'Director: ' + object1.Director + ', Actors: ' + object1.Actors + ', Rating ' + object1.Metascore;
               document.getElementById('boxOfDVDsDrama').appendChild(movieData)
@@ -124,17 +132,15 @@ function getApiDataImdb(event) {
     });
 
 }
-
-// function updateYear() {var movieYear = document.getElementById('enterYear').value;
-//  var scheduleContent0 = localStorage.getItem('enterYear');
-// enterYear.textContent = scheduleContent0;
-//   var entry0 = document.getElementById('enterYear').value;
-//  localStorage.setItem('enterYear', entry0);
-// movieYear.textContent = 
-// alert(movieYear)}
-// fetchOmdbButton.addEventListener('click', getApiOmdb);
+// requestPosterOmdb = 'http://img.omdbapi.com/?i=tt3896198&apikey=bf124b81&t=Remember%20the%20Titans&plot=full'
+// fetch(requestPosterOmdb)
+ //                .then(function(data) {var moviePoster = document.createElement('p');
+ //              var image = data; 
+ //            moviePoster.innerHTML = image;
+ //          document.getElementById('boxOfDVDsDrama').appendChild(moviePoster)})
 
 
+         
 
 // lines below are just to make sure that JS works here
 var pTag1 = document.createElement("p");
@@ -142,13 +148,5 @@ pTag1.textContent = 'this paragraph is created using JS';
 pTag1.setAttribute("id", "list1")
 pTag1.setAttribute("id", "parTag1")
 document.getElementById('question').appendChild(pTag1);
-var input1 = document.createElement("textarea");
-input1.setAttribute('id', 'year0')
-input1.setAttribute("class", "input")
-input1.setAttribute("placeholder", "year")
-document.getElementById('input2').appendChild(input1);
-var enterYear = localStorage.getItem('year0');
-var year2 = document.getElementById('year0').value;
-console.log(year2)
 
 
